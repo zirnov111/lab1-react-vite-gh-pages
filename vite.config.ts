a25c13f3b7869e3fdc1b699aa2c1ddf2a13f16d0
zirnov111 <zirnov111@gmail.com>
2025-12-07 22:32:50 +0300

    add lb3

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
     import { defineConfig } from 'vite'
-    import react from '@vitejs/plugin-react'; 
+    import react from '@vitejs/plugin-react';
 
     export default defineConfig({
       plugins: [react()],
-      base: '/lab1-react-vite-gh-pages', // Из Лабораторной работы 1
-      test: { // Конфигурация для Vitest
-        globals: true, // Позволяет использовать глобальные переменные (test, expect и т.д.) без явного импорта
-        environment: 'jsdom', // Использует JSDOM для эмуляции браузерного окружения
-        setupFiles: './src/setupTests.ts', // Файл для глобальных настроек тестов
-        css: true, // Включает обработку CSS в тестах (если ваши компоненты зависят от стилей)
-      } 
-        });
+      base: '/lab1-react-vite-gh-pages', 
+      // @ts-ignore
+      test: { 
+        globals: true, 
+        environment: 'jsdom', 
+        setupFiles: './src/setupTests.ts', 
+        css: true, 
+      }
+    });
